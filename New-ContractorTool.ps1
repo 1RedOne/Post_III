eb3ed4261e4505f50616f00fe3cb5508ef19662a
1RedOne <sred13@gmail.com>
2015-05-14 11:25:55 -0400

    Added New-ContractorTool
    Began working on the next set of UI elements to implement

--- New-ContractorTool.ps1
+++ New-ContractorTool.ps1
@@ -1,4 +1,6 @@
-﻿<Window x:Class="BlogPostIII.MainWindow"
+﻿    #ERASE ALL THIS AND PUT XAML BELOW between the @" "@ 
+$inputXML = @"
+<Window x:Class="BlogPostIII.MainWindow"
         xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
         xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
         xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
@@ -28,3 +30,49 @@
 
     </Grid>
 </Window>
+"@        
+
+$inputXML = $inputXML -replace 'mc:Ignorable="d"','' -replace "x:N",'N'  -replace '^<Win.*', '<Window'
+
+
+[void][System.Reflection.Assembly]::LoadWithPartialName('presentationframework')
+[xml]$XAML = $inputXML
+#Read XAML
+
+    $reader=(New-Object System.Xml.XmlNodeReader $xaml) 
+  try{$Form=[Windows.Markup.XamlReader]::Load( $reader )}
+catch{Write-Host "Unable to load Windows.Markup.XamlReader. Double-check syntax and ensure .net is installed."}
+
+#===========================================================================
+# Store Form Objects In PowerShell
+#===========================================================================
+
+$xaml.SelectNodes("//*[@Name]") | %{Set-Variable -Name "WPF$($_.Name)" -Value $Form.FindName($_.Name)}
+
+Function Get-FormVariables{
+if ($global:ReadmeDisplay -ne $true){Write-host "If you need to reference this display again, run Get-FormVariables" -ForegroundColor Yellow;$global:ReadmeDisplay=$true}
+write-host "Found the following interactable elements from our form" -ForegroundColor Cyan
+get-variable WPF*
+}
+
+Get-FormVariables
+
+#===========================================================================
+# Actually make the objects work
+#===========================================================================
+                                                               
+
+#Reference Sample entry of how to add data to a field
+    #$vmpicklistView.items.Add([pscustomobject]@{'VMName'=($_).Name;Status=$_.Status;Other="Yes"})
+    #$WPFtextBox.Text = $env:COMPUTERNAME
+    #$WPFbutton.Add_Click({$WPFlistView.Items.Clear();start-sleep -Milliseconds 840;Get-DiskInfo -computername $WPFtextBox.Text | % {$WPFlistView.AddChild($_)}  })
+
+#===========================================================================
+# Shows the form
+#===========================================================================
+write-host "To show the form, run the following" -ForegroundColor Cyan
+
+function Show-Form{
+$Form.ShowDialog() | out-null
+
+}
